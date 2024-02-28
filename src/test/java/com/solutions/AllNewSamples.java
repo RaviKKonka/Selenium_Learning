6458e1ab71db50fc5b835ccc25aebfe2cccda1ec
RaviKKonka <ravikonka76@gmail.com>
2024-02-28 15:29:07 -0600

    add examples

--- src/test/java/com/solutions/AllNewSamples.java
+++ src/test/java/com/solutions/AllNewSamples.java
@@ -1,5 +1,27 @@
 package com.solutions;
 
+import org.testng.annotations.Test;
+
 public class AllNewSamples {
 
+	@Test
+	void swapNumbers() {
+		int a = 20;
+		int b = 10;
+		b = (a+b)-(a=b);
+		System.out.println("a value >> " + a + " b value >> " + b);
+	}
+	
+	@Test
+	void reverseNumber() {
+		int num = 123456;
+		int rev = 0;
+		while(num>0) {
+			rev = rev*10 + num%10;
+			num = num/10;
+		}
+		System.out.println(rev);
+	}
+	
+	
 }
